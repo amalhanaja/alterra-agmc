8251270f584e046f7ea6a97f4c3ad6c2b13de471
amalhanaja <amalhanaja@gmail.com>
2022-09-21 22:36:20 +0700

    feat: add response

--- day-5-6/internal/dto/user.go
+++ day-5-6/internal/dto/user.go
@@ -1,5 +1,7 @@
 package dto
 
+import "time"
+
 type LoginUserPaylaod struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
@@ -15,4 +17,12 @@ type UpdateUserPayload struct {
 	Name     string `json:"name,omitempty" validate:"omitempty"`
 	Email    string `json:"email,omitempty" validate:"omitempty,email"`
 	Password string `json:"password,omitempty" validate:"omitempty,min=8"`
+}
+
+type UserResponse struct {
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
